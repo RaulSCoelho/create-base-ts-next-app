de2f5f974d53817573ec66aab912820dad54d64c
StartShows <talktostart@gmail.com>
2022-12-01 23:05:15 -0300

    random user

--- src/context/UserContext.tsx
+++ src/context/UserContext.tsx
@@ -1,4 +1,4 @@
-import { createContext, useEffect, useState } from 'react'
+import { createContext, useCallback, useEffect, useState } from 'react'
 
 import { faker } from '@faker-js/faker'
 
@@ -14,19 +14,17 @@ type User = {
 type UserContextType = {
   isAuthenticated: boolean
   user: User
+  randomUser: User
 }
 
 export const UserContext = createContext({} as UserContextType)
 
 export const UserProvider = ({ children }) => {
   const [user, setUser] = useState<User>(null)
+  const [randomUser, setRandomUser] = useState<User>(null)
   const isAuthenticated = !!user
 
-  useEffect(() => {
-    setUser(getFakeUser())
-  }, [])
-
-  function getFakeUser(): User {
+  const getFakeUser = useCallback((): User => {
     faker.seed(12895671)
     const user: User = {
       id: faker.datatype.uuid(),
@@ -38,10 +36,29 @@ export const UserProvider = ({ children }) => {
     }
 
     return user
-  }
+  }, [])
+
+  const getRandomFakeUser = useCallback((): User => {
+    faker.seed(Math.random() * 10000)
+    const user: User = {
+      id: faker.datatype.uuid(),
+      name: faker.name.fullName(),
+      username: faker.internet.userName(),
+      email: faker.internet.email(),
+      birthday: faker.date.birthdate(),
+      avatar_url: faker.image.avatar()
+    }
+
+    return user
+  }, [])
+
+  useEffect(() => {
+    setUser(getFakeUser())
+    setRandomUser(getRandomFakeUser())
+  }, [getFakeUser, getRandomFakeUser])
 
   return (
-    <UserContext.Provider value={{ isAuthenticated, user }}>
+    <UserContext.Provider value={{ isAuthenticated, user, randomUser }}>
       {children}
     </UserContext.Provider>
   )
